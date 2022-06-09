8d0f28557072d9d5b126e697d2a44016282af69b
Hugo Castro <hreisc@gmail.com>
2022-06-09 02:29:30 +0100

    Added action_add_to_cart.php

---
+++ actions/action_add_to_cart.php
@@ -0,0 +1,36 @@
+<?php
+
+    declare(strict_types = 1);
+
+    session_start();
+
+    require_once('../database/connection.db.php');
+    require_once('../database/restaurant.class.php');
+    require_once('../database/dish.class.php');
+
+    $db = getDatabaseConnection();
+
+    $idDish = intval($_GET['id']);		 //enviado pelo link da pagina
+    $quantity = intval($_POST['quantity']);
+
+    $dish = Dish::getDish($db, intval($idDish));
+
+    if (!isset($_SESSION['cart'])){
+        $_SESSION ['idRestaurant'] = $dish->idRestaurant;
+    }
+
+    if ($dish->idRestaurant != $_SESSION ['idRestaurant']){?>
+        <script>
+            //TO DO MENSAGEM DE ALERTA JAVASCRIPT
+        </script>
+        <?php header('Location: ../pages/restaurant.php?id='.$_POST['idRestaurant']);
+    }
+
+    if(!isset($_SESSION['cart'][$idDish])) {
+        $_SESSION['cart'][$idDish] = 0;	
+    }
+
+    $_SESSION['cart'][$idDish] += $quantity;
+
+    header('Location: ../pages/restaurant.php?id='.$_POST['idRestaurant']);
+?>
